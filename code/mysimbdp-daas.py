50c6301a152c752f5609a8d430892df21537a0b5
alvarorgaz <alvarorgaz@gmail.com>
2019-10-08 16:53:39 +0300

    Modify mysimbdp-daas.py to ingest request using API of MongoDB.

--- code/mysimbdp-daas.py
+++ code/mysimbdp-daas.py
@@ -1,10 +1,15 @@
+import pymongo
 from flask import Flask, request
+
+mongo_client = pymongo.MongoClient('mongodb+srv://alvarorgaz:XLao4jEcoIz3kFXH@big-data-a1-j25ko.gcp.mongodb.net/admin?retryWrites=true&w=majority')
+database = mongo_client["google_play_store"]
+table = database["apps"]
 
 app = Flask(__name__)
 
 @app.route('/ingestion', methods=['POST'])
 def ingestion():
-	print(request.json)
+	table.insert(request.json)
 	return ""
 
 app.run(host="0.0.0.0", port=80)
